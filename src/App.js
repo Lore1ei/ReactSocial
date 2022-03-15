ed3da19000fee507b8b6c8d6b04cc2a56e6bd00d
Your Name <you@example.com>
2022-03-15 19:16:33 +0700

    s

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='first'>
+        <Header />
+        <Technologies />
+        <Footer />
+  </div>
+
+  );
+}
+
+
+// const Footer = () => {
+//   return(
+//     <div className="class">
+//       Подвал
+//     </div>
+//   );
+// }
+
+export default App;
---
+++ src/header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className='second'>
+        <p>Hello</p>
+  </div>
+  );
+}
+
+export default Header;

89e7f9d670c8c80324f3492f829f3c0353d9e509
Your Name <you@example.com>
2022-03-17 20:07:42 +0700

    sd

--- src/components/Heaader.jsx
+++ src/components/Heaader.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return(
         <header className='header'>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="" />s
         </header>   
     )
 }

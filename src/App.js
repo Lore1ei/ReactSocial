ff1cea951470bbd70116405b50415263f41a7a82
Your Name <you@example.com>
2022-03-17 19:37:20 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './main.css';
+import Header from './components/Heaader';
+import Nav from './components/Nav';
+import Profile from './components/Profile';
+
+
 
 const App = () => {
   return (
-    <div className='first'>
-        <Header />
-        <Technologies />
-        <Footer />
+    <div className='app-wrapper'>
+    <Header />
+    <Nav />
+    <Profile />
+      
   </div>
 
   );
--- src/header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <div className='second'>
-        <p>Hello</p>
-  </div>
-  );
-}
-
-export default Header;

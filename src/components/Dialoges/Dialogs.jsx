d63300df78252ec17bad4d87d413bb9cece29940
Lore1ei <you@example.com>
2022-04-05 20:37:51 +0700

    refactoring add object

--- src/components/Dialoges/Dialogs.jsx
+++ src/components/Dialoges/Dialogs.jsx
@@ -15,20 +15,34 @@ const Message = (props) =>{
 }
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        {path: 1, name: 'Alexandr'},
+        {path: 2, name: 'Misha'},
+        {path: 3, name: 'Andrey'},
+        {path: 4, name: 'Igor'},
+        {path: 5, name: 'Tatyana'},
+        {path: 6, name: 'Oleg'},
+    ];
+
+    let messagesData = [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'Hi, How are you today?'},
+        {id: 3, message: 'Nice to meet you!'},
+        {id: 4, message: 'Hello'},
+        {id: 5, message: 'Hello'},
+        {id: 6, message: 'Hello'},
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name="Alexander" path="1"/>
-                <DialogItem name="Misha" path="2"/>
-                <DialogItem name="Andrey" path="3"/>
-                <DialogItem name="Igor" path="4"/>
-                <DialogItem name="Tatyana" path="5"/>
-                <DialogItem name="Oleg" path="6"/>
+                <DialogItem name={dialogsData[0].name} path={dialogsData[0].path}/>
+                <DialogItem name={dialogsData[1].name} path={dialogsData[1].name}/>
             </div>
             <div className={s.messages}>
-                <Message message='Hello'/>
-                <Message message='Hi, How are you today?'/>
-                <Message message='Nice to meet you!'/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,12 @@ import profile from './MyPosts.module.css';
 import Post from './posts/Post';
 
 const Myposts = () => {
+
+    let mypostsData = [
+        {id: 1, message: 'Hello', countlike: 11},
+        {id: 2, message: 'Holla', countlike: 23},
+    ]
+
     return(
               <div className={profile.posts}>
                 <div className={profile.name_post}>My posts</div>
@@ -15,8 +21,8 @@ const Myposts = () => {
                 </div>
 
                 <div className={profile.posts__form}>
-                  <Post message='Hello' countlike='11'/>
-                  <Post message='Holla' countlike='23'/>
+                  <Post message={mypostsData[0].message} countlike={mypostsData[0].countlike}/>
+                  <Post message={mypostsData[1].message} countlike={mypostsData[1].countlike}/>
                 </div>
               </div>
     )

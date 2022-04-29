56a4eb0b4448cf9d0567d84ad2f6d9029446b542
Lore1ei <you@example.com>
2022-04-29 20:43:08 +0700

    dispatch e.target

--- src/components/Dialoges/Dialogs.jsx
+++ src/components/Dialoges/Dialogs.jsx
@@ -9,16 +9,12 @@ const Dialogs = (props) => {
     let dialogsElements = props.state.dialogs.map( d => <DialogItem name={d.name} path={d.path} img={d.img}/>)
     let messagesElements = props.state.messages.map( m => <Message message={m.message}/>)
 
-    let ref = React.createRef();
-
     let addMessage = () => {
-        if(ref.current.value.length >= 1){
             props.dispatch(AddMessageDialogActionCreator());
-        }
     };
 
-    let dialogOnChange = () => {
-        let text = ref.current.value;
+    let dialogOnChange = (e) => {
+        let text = e.target.value;
         props.dispatch(ChangeDialogTextActionCreator(text));
     }
 
@@ -30,7 +26,7 @@ const Dialogs = (props) => {
             <div className={s.messages}>
                 { messagesElements }
 
-                <textarea ref={ref} onChange={dialogOnChange} name="" id="" cols="30" rows="5" value={props.state.newPost}/>
+                <textarea onChange={dialogOnChange} name="" id="" cols="30" rows="5" value={props.state.newPost}/>
                 <button className={s.btn} onClick={addMessage}>Push Message</button>
             </div>
         </div>
